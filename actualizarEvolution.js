eef96495cdde2ce9b8a328cb9014b6bc99bee7ee
TTFH <ttfh3500@gmail.com>
2025-08-07 10:49:17 -0300

    Generalizar codigo

--- actualizarEvolution.js
+++ actualizarEvolution.js
@@ -7,7 +7,6 @@ const chatwoot_url = process.env.CHATWOOT_URL;
 const api_access_token = process.env.API_ACCESS_TOKEN;
 
 const CHATWOOT_URL_PREFIX = `${chatwoot_url}/api/v1/accounts/2`;
-const INBOX_ID = 32;
 
 const chatwoot = axios.create({
 	baseURL: CHATWOOT_URL_PREFIX,
@@ -17,9 +16,9 @@ const chatwoot = axios.create({
 	},
 });
 
-async function UpdateContact(contactId, phone) {
+async function UpdateContactIdentifier(contactId, newIdentifier) {
 	const contact = {
-		identifier: phone + "@s.whatsapp.net",
+		identifier: newIdentifier,
 	};
 	try {
 		const response = await chatwoot.put(`/contacts/${contactId}`, contact);
@@ -30,21 +29,18 @@ async function UpdateContact(contactId, phone) {
 	}
 }
 
-async function GetConversations(page) {
+async function GetConversationsFromInbox(inbox_id, page) {
 	const conversation = {
 		payload: [
 			{
 				attribute_key: "inbox_id",
 				filter_operator: "equal_to",
-				values: [INBOX_ID],
+				values: [inbox_id],
 			},
 		],
 	};
 	try {
-		const response = await chatwoot.post(
-			`/conversations/filter?page=${page}`,
-			conversation
-		);
+		const response = await chatwoot.post(`/conversations/filter?page=${page}`, conversation);
 		return response.data.payload;
 	} catch (error) {
 		console.error("Error al obtener conversaciones", error.message);
@@ -55,18 +51,18 @@ async function GetConversations(page) {
 async function main() {
 	let page = 1;
 	let conversations = [];
+	const INBOX_ID = 32;
 	do {
 		console.log(`Procesando página ${page}`);
-		conversations = await GetConversations(page);
+		conversations = await GetConversationsFromInbox(INBOX_ID, page);
 		for (const conversation of conversations) {
-			//const conversationId = conversation.id;
 			const contactId = conversation.meta.sender.id;
 			const contactIdentifier = conversation.meta.sender.identifier;
 			const contactPhone = conversation.meta.sender.phone_number.replace("+", "");
-			if (contactIdentifier !== contactPhone + "@s.whatsapp.net") {
-				const updatedContact = await UpdateContact(contactId, contactPhone);
-				if (updatedContact)
-					console.log(`Contacto ${contactId} / ${contactPhone} actualizado`);
+			const newIdentifier = contactPhone + "@s.whatsapp.net";
+			if (contactIdentifier !== newIdentifier) {
+				const updatedContact = await UpdateContactIdentifier(contactId, newIdentifier);
+				if (updatedContact) console.log(`Contacto ${contactId} / ${contactPhone} actualizado`);
 			}
 		}
 		page++;
