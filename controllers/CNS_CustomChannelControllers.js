d9ca670abc4f32d27a1f877f287ff8b4c6a7c25f
TTFH <ttfh3500@gmail.com>
2025-08-12 11:42:23 -0300

    Convertir id a string

--- controllers/CNS_CustomChannelControllers.js
+++ controllers/CNS_CustomChannelControllers.js
@@ -38,11 +38,11 @@ async function GetContactId(contact) {
 
 	const payload = {
 		payload: [
-			{ id: contact.id, key: "identifier" },
-			{ id: contact.email, key: "email" },
-			{ id: contact.phone, key: "phone_number" }
+			{ value: contact.id.toString(), key: "identifier" },
+			{ value: contact.email, key: "email" },
+			{ value: contact.phone, key: "phone_number" }
 		]
-			.map(item => buildPayloadItem(item.key, item.id))
+			.map(item => buildPayloadItem(item.key, item.value))
 			.filter(Boolean)
 			.map((item, index, array) => ({
 				...item,
