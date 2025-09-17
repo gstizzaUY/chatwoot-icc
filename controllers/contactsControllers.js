79fe184b5bf2b483c449debcf37aaf9ca648c123
Guillermo Stizza <gstizza1@vera.com.uy>
2025-09-17 08:35:21 -0300

    arreglo al crear contacto en chatwoot, no encontraba numero de telefono

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -515,7 +515,7 @@ const createContact = async (req, res) => {
         "name": contact.firstname + ' ' + contact.lastname,
         "inbox_id": contact.phone !== null ? 23 : 1,  
         "email": contact.email,
-        "phone_number": contact.phoneInternational,
+        "phone_number": contact.phone,
         "custom_attributes": {
             "firstname": contact.firstname,
             "lastname": contact.lastname,
