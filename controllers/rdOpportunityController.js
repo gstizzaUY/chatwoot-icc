9ec27db9237dac6c1b3c52a348711afa4d644046
TTFH <ttfh3500@gmail.com>
2025-10-27 11:08:06 -0300

    obtener oportunidad V1

--- controllers/rdOpportunityController.js
+++ controllers/rdOpportunityController.js
@@ -3,124 +3,64 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const RDSTATION_URL = process.env.RDSTATION_URL;
-const RDSTATION_CLIENT_ID = process.env.RDSTATION_OPPORTUNITY_CLIENT_ID;
-const RDSTATION_CLIENT_SECRET = process.env.RDSTATION_OPPORTUNITY_CLIENT_SECRET;
-const RDSTATION_REFRESH_TOKEN = process.env.RDSTATION_OPPORTUNITY_REFRESH_TOKEN;
+const RDSTATION_CRM_URL = process.env.RDSTATION_CRM_URL;
+const RDSTATION_USER_TOKEN = process.env.RDSTATION_USER_TOKEN;
 
 const rdstation = axios.create({
-	baseURL: RDSTATION_URL,
+	baseURL: RDSTATION_CRM_URL,
+	params: { token: RDSTATION_USER_TOKEN },
 	headers: {
 		"Content-Type": "application/json"
 	}
 });
 
-function SetAccessToken(token) {
-	rdstation.defaults.headers["Authorization"] = `Bearer ${token}`;
-}
-
-async function UpdateAccessToken() {
-	const credentials = {
-		grant_type: "refresh_token",
-		client_id: RDSTATION_CLIENT_ID,
-		client_secret: RDSTATION_CLIENT_SECRET,
-		refresh_token: RDSTATION_REFRESH_TOKEN
-	};
-	try {
-		const response = await rdstation.post("/oauth2/token", credentials);
-		return response.data.access_token;
-	} catch (error) {
-		console.error("Error al actualizar token", error.message);
-		return null;
-	}
-}
-
 function GenerateContactId(phone) {
+	if (!phone) return null;
 	return `${phone.replace(/\D/g, "")}@email.com`;
 }
 
-async function GetContactID(phone, email) {
+async function GetContactCRM(phone, email) {
 	try {
 		if (email) {
-			const filter = encodeURIComponent(`email:${email}`);
-			const response = await rdstation.get(`/crm/v2/contacts?filter=${filter}`);
-			if (response.data.data && response.data.data.length > 0)
-				return response.data.data[0].id;
+			const filter = encodeURIComponent(`${email}`);
+			const response = await rdstation.get(`/api/v1/contacts?email=${filter}`);
+			if (response.data.total > 0)
+				return response.data.contacts[0];
 		}
 		const email2 = GenerateContactId(phone);
-		const filter = encodeURIComponent(`email:${email2}`);
-		const response = await rdstation.get(`/crm/v2/contacts?filter=${filter}`);
-		if (response.data.data && response.data.data.length > 0)
-			return response.data.data[0].id;
+		const filter = encodeURIComponent(`${email2}`);
+		const response = await rdstation.get(`/api/v1/contacts?email=${filter}`);
+		if (response.data.total > 0)
+			return response.data.contacts[0];
 	} catch (error) {
-		if (error.response && error.response.status === 401) throw new Error("INVALID_TOKEN");
-		console.error("Error al obtener id contacto en crm", JSON.stringify(contactData), error.message);
+		console.error("Error al obtener contacto en crm", error.message);
 		return null;
-	}
-	return null;
-}
-
-async function GetContactDeal(contactId) {
-	var page = 1;
-	while (true) {
-		const response = await rdstation.get(`/crm/v2/deals?page[number]=${page}&page[size]=50`);
-		if (!response.data.data || response.data.data.length === 0)
-			break;
-		for (const deal of response.data.data) {
-			console.log("Checking deal:", deal.id, "for contact ID:", contactId);
-			if (deal.id === contactId)
-				return deal;
-		}
-		page++;
 	}
 	return null;
 }
 
 async function FetchOpportunity(phone, email) {
-	console.log("Fetching contact ID for:", phone, email);
-	const contactId = await GetContactID(phone, email);
-	console.log("Contact ID:", contactId);
-	if (!contactId) return null;
-	var deal = await GetContactDeal(contactId);
-	console.log("Deal:", deal);
-	if (!deal) return null;
+	var contact = await GetContactCRM(phone, email);
+	if (!contact) return null;
 
-	const stageId = deal.stage_id;
-	const pipelineId = "68d14bbd5a3017001e7e3a0c";
-
-	const response = await rdstation.get(`/crm/v2/pipelines/${pipelineId}/stages/${stageId}`);
-	const stage = response.data.data;
-	console.log("Stage:", stage);
-	deal = {
-		stage_name: stage.name,
-		stage_description: stage.description,
-		stage_objective: stage.objective,
-		...deal,
+	for (let i = 0; i < contact.deals.length; i++) {
+		const dealId = contact.deals[i].id;
+		try {
+			const response = await rdstation.get(`/api/v1/deals/${dealId}`);
+			contact.deals[i] = response.data;
+		} catch (error) {
+			console.error("Error al obtener oportunidad en crm", error.message, dealId);
+			continue;
+		}
 	}
-	return deal;
+	return contact;
 }
 
 async function GetOpportunityRD(req, res) {
 	const email = req.query.email;
 	const phone = req.query.phone;
-	console.log("Fetching opportunity for:", phone, email);
-	try {
-		const opportunity = await FetchOpportunity(phone, email);
-		if (opportunity) return res.status(200).json(opportunity);
-	} catch (error) {
-		try {
-		console.error("Error fetching opportunity:", error.message);
-		if (error.message === "INVALID_TOKEN") {
-			console.log("Getting new access token");
-			const token = await UpdateAccessToken();
-			SetAccessToken(token);
-			const opportunity = await FetchOpportunity(phone, email);
-			if (opportunity) return res.status(200).json(opportunity);
-		}
-		} catch (error2) {
-			return res.status(500).send("Failed to update token");
-		}
-	}
+	const opportunity = await FetchOpportunity(phone, email);
+	if (opportunity) return res.status(200).json(opportunity);
 	return res.status(404).send("Opportunity not found");
 }
 
--- routes/conversationsRoutes.js
+++ routes/conversationsRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { NotifyOutgoingMessage } from "../controllers/conversationsControllers.js";
 import { OnOutgoingWppMessage, OnSolvedWppConversation } from "../controllers/chatbotControllers.js";
 import { OnNewContact, GetContactRD, UpdateContactRD, RegisterContact } from "../controllers/registerContactController.js";
+import { GetOpportunityRD } from "../controllers/rdOpportunityController.js";
 
 const router = express.Router();
 
@@ -10,6 +11,7 @@ router.post("/outgoing-wpp-message", OnOutgoingWppMessage); // Agente
 router.post("/new-contact", OnNewContact); // Registrar en RD Station
 router.post("/solved-wpp-conversation", OnSolvedWppConversation); // Cerrar conversación
 router.get("/get-contact", GetContactRD); // Proxy obtener contacto RD Station
+router.get("/get-opportunity", GetOpportunityRD); // Obtener oportunidad RD Station
 router.post("/update-contact", UpdateContactRD); // Actualizar contacto RD Station
 router.post("/register-contact", RegisterContact); // Registrar usuario iChef
 
